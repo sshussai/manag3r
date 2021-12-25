f53d7371922a84529175f4f272340ffb18f516b6
sshussai <sshussai@ualberta.ca>
2021-12-25 09:56:20 -0700

    update task status to a select field

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :status, inclusion: { in: %w(not-completed in-progress completed) }
+
+  STATUS_OPTIONS = [
+    ['Not competed', 'not-completed'],
+    ['In progress', 'in-progress'],
+    ['Competed', 'completed']
+  ]
+
 end
