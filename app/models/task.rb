3bb537e3fbb840d3054bed18c9f24d727936b744
sshussai <sshussai@ualberta.ca>
2021-12-28 00:38:32 -0700

    fix redirect after task creation, and task status value

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :project
 
-  validates :status, inclusion: { in: %w(not-completed in-progress completed) }
+  validates :status, inclusion: { in: %w(not-started in-progress completed) }
 
   STATUS_OPTIONS = [
-    ['Not competed', 'not-completed'],
+    ['Not completed', 'not-started'],
     ['In progress', 'in-progress'],
     ['Competed', 'completed']
   ]
